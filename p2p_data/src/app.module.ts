5286faafbb7b73465881a6f8619f566fcc312808
Andhromede <andhromede@hotmail.fr>
2023-06-20 11:34:23 +0200

    add user module, controller and module

--- p2p_data/src/app.module.ts
+++ p2p_data/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './modules/user.module';
+import { UserController } from './controllers/user.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [UserModule],
+  controllers: [AppController, UserController],
   providers: [AppService],
 })
 export class AppModule {}
