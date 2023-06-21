36ad1f9ae6e348b51a3da32a10ac9664a80b8126
Andhromede <andhromede@hotmail.fr>
2023-06-21 13:04:14 +0200

    feat: update app.module connexion

--- p2p_data/src/app.module.ts
+++ p2p_data/src/app.module.ts
@@ -5,29 +5,13 @@ import { UserModule } from './user/user.module';
 import { RoleModule } from './role/role.module';
 import { PermissionModule } from './permission/permission.module';
 import { UserTrainingModule } from './user-training/user-training.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { TrainingModule } from './training/training.module';
 import { ChapterModule } from './chapter/chapter.module';
+
 
 @Module({
     controllers: [AppController],
     providers: [AppService],
-    imports: [UserModule, RoleModule, PermissionModule, UserTrainingModule,
-        TypeOrmModule.forRoot({
-            type: 'postgres',
-            host: 'localhost',
-            port: 5432,
-            username: 'Nath',
-            password: '1234',
-            database: 'p2p',
-            entities: [__dirname + '/**/*.entity{.ts,.js}'],
-            synchronize: true,
-            // extra: {
-            //     ssl: true
-            // }
-        }),
-        TrainingModule,
-        ChapterModule,
-    ],
+    imports: [UserModule, RoleModule, PermissionModule, UserTrainingModule, TrainingModule, ChapterModule ],
 })
 export class AppModule { }
