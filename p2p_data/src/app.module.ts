91afadaaf31e4981882db6a750a5e4504c301523
VictorMauroy <mauroy.victor@gmail.com>
2023-06-21 16:23:23 +0200

    Fix: :bug: resolve connection error and add database synchronisation

--- p2p_data/src/app.module.ts
+++ p2p_data/src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RoleModule } from './role/role.module';
@@ -13,6 +14,20 @@ import { PersonModule } from './person/person.module';
     providers: [AppService],
     imports: [
         ConfigModule.forRoot(),
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],      
+            useFactory: (configService: ConfigService) => ({
+              type: 'postgres' as 'postgres',
+              host: configService.get<string>('DB_HOST'),
+              port: parseInt(configService.get<string>('DB_PORT')),
+              username: configService.get<string>('DB_USERNAME'),
+              password: configService.get<string>('DB_PASSWORD'),
+              database: configService.get<string>('DB_NAME'),
+              entities: [__dirname + '/**/*.entity{.ts,.js}'],
+              synchronize: true,
+            }),
+            inject: [ConfigService],
+          }),
         RoleModule, PermissionModule, TrainingModule, ChapterModule, PersonModule ],
 })
 export class AppModule { }
