05d49c4dfd9b1fc54c5a220f1ef5a20df4849b57
VictorMauroy <mauroy.victor@gmail.com>
2023-06-21 15:23:13 +0200

    Fix: :bug: fix connection to database

--- p2p_data/src/app.module.ts
+++ p2p_data/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RoleModule } from './role/role.module';
@@ -6,11 +7,13 @@ import { PermissionModule } from './permission/permission.module';
 import { TrainingModule } from './training/training.module';
 import { ChapterModule } from './chapter/chapter.module';
 import { PersonModule } from './person/person.module';
-
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
     controllers: [AppController],
     providers: [AppService],
-    imports: [RoleModule, PermissionModule, TrainingModule, ChapterModule, PersonModule ],
+    imports: [
+        ConfigModule.forRoot(),
+        RoleModule, PermissionModule, TrainingModule, ChapterModule, PersonModule ],
 })
 export class AppModule { }
