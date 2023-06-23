cbf3da9327637994c8f90f85b9424028cb868202
Andhromede <andhromede@hotmail.fr>
2023-06-23 15:03:45 +0200

    feat: person methodes are done

--- p2p_data/src/person/person.controller.ts
+++ p2p_data/src/person/person.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Param, Req } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Req, Put, Delete } from '@nestjs/common';
 import { PersonService } from './person.service';
 import { Person } from '../entities/person.entity';
 
@@ -7,36 +7,61 @@ export class PersonController {
 
     constructor(private readonly personService: PersonService) { }
 
+    // Search all users
     @Get()
-    GetAllPersons(){
+    GetAllPersons() {
         return this.personService.GetAllPersons();
     }
 
+    // Search one user by ID
     @Get('id/:id')
-    GetPersonById(@Param('id') personId : number) {
+    GetPersonById(@Param('id') personId: number) {
         return this.personService.GetPersonById(personId);
     }
 
+    // Search one users by EMAIL
     @Get('email')
     GetPersonByEmail(@Req() req) {
         const email = req.body.email;
         return this.personService.GetPersonByEmail(email);
     }
 
+    // Create one user if didn't exist
     @Post()
     async createPerson(@Req() req) {
         const lastName = req.body.lastName;
         const firstName = req.body.firstName;
         const email = req.body.email;
         const password = req.body.password;
-        const adresse = req.body.adresse;
+        const adress = req.body.adress;
         const birthday = req.body.birthday;
         const isActive = req.body.isActive;
-        return this.personService.createPerson(lastName,firstName,email,password,adresse,birthday,isActive);
+
+        return this.personService.createPerson(lastName, firstName, email, password, adress, birthday, isActive);
     }
 
+    // Update one users
+    @Put(':id')
+    async updatePerson(@Param('id') personId: number, @Req() req): Promise<Person> {
+        
+        const lastName = req.body.lastName;
+        const firstName = req.body.firstName;
+        const email = req.body.email;
+        const password = req.body.password;
+        const adress = req.body.adress;
+        const birthday = req.body.birthday;
+        const isActive = req.body.isActive;
 
+        const updatedPerson = await this.personService.updatePerson(
+            personId, lastName, firstName, email, password, adress, birthday, isActive
+        );
+        return updatedPerson;
+    }
 
-
-
+    // Delete one users
+    @Delete(':id')
+    async deletePerson(@Param('id') personId: number): Promise<string> {
+        const deletedPerson = await this.personService.deletePerson(personId);
+        return deletedPerson;
+    }
 }
--- p2p_data/src/person/person.repository.ts
+++ p2p_data/src/person/person.repository.ts
@@ -10,36 +10,93 @@ export class PersonRepository {
     constructor(private dataSource: DataSource) { }
     personRepository = this.dataSource.getRepository(Person);
 
+    // Search all users
     async GetAllPersons(): Promise<Person[]> {
-        return await this.personRepository.find();
+        try {
+            return await this.personRepository.find()
+        } catch (error) {
+            return error;
+        }
     }
 
+    // Search one user by ID
     async GetPersonById(personId: number): Promise<Person> {
-        return await this.personRepository.findOneBy({
-            id: personId
-        });
+        try {
+            return await this.personRepository.findOneBy({
+                id: personId
+            });
+        } catch (error) {
+            return error;
+        }
+
     }
 
+    // Search one users by EMAIL
     async GetPersonByEmail(email: string): Promise<Person> {
-        return await this.personRepository.findOneBy({
-            email: email
-        });
+        try {
+            return await this.personRepository.findOneBy({
+                email: email
+            });
+        } catch (error) {
+            return error;
+        }
+
     }
 
+    // Create one user if didn't exist
     async CreatePerson(
-        lastName: string, 
-        firstName: string, 
+        lastName: string,
+        firstName: string,
         email: string,
-        password: string, 
-        adress: string, 
-        birthday: string, 
+        password: string,
+        adress: string,
+        birthday: Date,
+        isActive: boolean
+    ): Promise<Person> {
+        try {
+            const person = await this.personRepository.create(
+                { firstName, lastName, email, password, adress, birthday, isActive }
+            );
+            return this.personRepository.save(person);
+        } catch (error) {
+            return error;
+        }
+    }
+
+    // Update one users
+    async updatePerson(
+        personId: number,
+        lastName: string,
+        firstName: string,
+        email: string,
+        password: string,
+        adress: string,
+        birthday: Date,
         isActive: boolean
     ): Promise<Person> {
 
-        const person = await this.personRepository.create(
-            { firstName, lastName, email, password, adress, birthday, isActive }
-        );
-        return this.personRepository.save(person);
+        try {
+            const person = await this.personRepository.findOneBy({ id: personId });
+            person.lastName = lastName;
+            person.firstName = firstName;
+            person.email = email;
+            person.password = password;
+            person.adress = adress;
+            person.birthday = birthday;
+            person.isActive = isActive;
+            return this.personRepository.save(person);
+        } catch (error) {
+            return error;
+        }
     }
 
+    // Delete one users
+    deletePerson(personId: number) {
+        try {
+            this.personRepository.delete(personId);
+            return "Person is deleted";
+        } catch (error) {
+            return error;
+        }
+    }
 }
